f02db6e4da515009988227e7c247e0d5baca2a49
Nyareh David <70676816+NyarehD@users.noreply.github.com>
2022-10-19 15:58:56 +0630

    Added: Navbar to layout

--- delivery-food-dashboard/pages/index.tsx
+++ delivery-food-dashboard/pages/index.tsx
@@ -1,34 +1,33 @@
 // Utils
-import type { NextPage } from "next"
-import { useEffect, useState } from "react"
-import axios, { AxiosResponse } from "axios"
+import type {NextPage} from "next"
+import axios, {AxiosResponse} from "axios"
 // Component and Styles
-import Head from "next/head"
-import Image from "next/image"
-import styles from "../styles/Home.module.css"
 import DataTable from "components/data-table"
 // Types
-import type { Rider } from "types/data.types"
-import type { HomePageProps } from "types/home-page-props.types"
+import type {Rider} from "types/data.types"
+import type {HomePageProps} from "types/home-page-props.types"
+import Layout from "../components/Layout/Layout";
 
-const Home: NextPage<HomePageProps> = ({ riders }) => {
+const Home: NextPage<HomePageProps> = ({riders}) => {
     console.log(riders)
     return (
-        <div className={styles.container}>
+        <Layout name="Cat is Cool">
             <DataTable
                 headerData={riders!.length ? Object.keys(riders![0]) : []}
-                bodyData={{ riders }}
+                bodyData={{riders}}
             />
-        </div>
+        </Layout>
     )
 }
+
 export async function getStaticProps() {
     const response: AxiosResponse = await axios.get(
         "http://localhost:3001/riders"
     )
     const data: Rider[] = Object.values(response.data)
     return {
-        props: { riders: [...data] },
+        props: {riders: [...data]},
     }
 }
+
 export default Home
