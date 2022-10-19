dd7c82cd0b5c3f04edae82482711fbc66d5a1e54
Nyareh David <70676816+NyarehD@users.noreply.github.com>
2022-10-19 16:19:45 +0630

    Added: Entities section in Layout.tsx

--- delivery-food-dashboard/components/Layout/Layout.tsx
+++ delivery-food-dashboard/components/Layout/Layout.tsx
@@ -9,6 +9,16 @@ export default function Layout({name, children}: LayoutProps) {
     return (
         <div className={layoutStyle.container}>
             <NavBar name={name}/>
+            <div className={layoutStyle.entityContainer}>
+                <h1>Entities</h1>
+                <ul>
+                    <li>Customers</li>
+                    <li>Orders</li>
+                    <li>Foods</li>
+                    <li>Riders</li>
+                    <li>Shops</li>
+                </ul>
+            </div>
             {children}
         </div>
     )
